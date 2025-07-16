49e6a29cc33b47a0e808bb272981c450ad8eb69e
Ishan Shrestha <iahan.esan18@gmail.com>
2025-07-16 20:19:39 +0545

    authdto

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,19 +1,22 @@
-import { IsString } from "class-validator";
+import { IsEmail, IsString, MinLength } from "class-validator";
 import { Column } from "typeorm";
 
-export class RgisterDto{
-   @Column()
+export class RegisterDto {
+    @Column()
     id: string
 
     @IsString()
     fullName: string
 
-    
-        @IsString()
-        phone: string
-    
-        @IsString()
-        password: string
+    @IsEmail()
+    email: string
+
+    @IsString()
+    phone: string
+
+    @IsString()
+    @MinLength(6)
+    password: string
 
 
 }
